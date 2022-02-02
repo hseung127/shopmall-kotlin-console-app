64918b22fa11c4f5399c74a72ca1826a03b087b3
rubat0 <rubato00@naver.com>
2022-02-03 00:46:41 +0900

    단계3. main 함수의 구현을 클래스 함수로 분리

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+import screen.ShoppingHome
+
+/*
+ * (1) main함수 -> Class 함수
+ * (2) 화면별 클래스 분리 -> 홈, 카테고리화면
+ * (3) 선택한 카테고리 상품목록 표시
+ */
+fun main(args: Array<String>) {
+    val shoppingHome = ShoppingHome()
+
+    shoppingHome.start()
+}
