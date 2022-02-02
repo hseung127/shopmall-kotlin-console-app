4f1fb54dfcb93362dfc0cc86385290cfc4cae231
rubat0 <rubato00@naver.com>
2022-02-03 00:15:55 +0900

    단계 3. main 함수의 구현을 클래스 함수로 분리

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,55 +1,12 @@
-fun main(args:Array<String>){
-    //1인사말 출력
+import screen.ShoppingHome
 
-    //2사용자 이름 입력 받기
+/*
+ * (1) main함수 -> Class 함수
+ * (2) 화면별 클래스 분리 -> 홈, 카테고리화면
+ * (3) 선택한 카테고리 상품목록 표시
+ */
+fun main(args: Array<String>) {
+    val shoppingHome = ShoppingHome()
 
-    //3입력받은 사용자 이름 출력
-    println("안녕하세요 , shopmall 에 오신 것을 환영 합니다 !")
-    println("쇼핑을 게속 하시려면 이름을 입력해주세요:")
-
-    val name = readLine() //g화면에서 사용자 입력 받기
-
-    //"""는 화면에 그대로 표시
-    println("""  
-        감사합니다. 반갑습니다. $name 님
-        원하시는 카테고리를 입력해주세요.
-        ***==========================***
-    """.trimIndent())
-
-    /*
-    * 1  상품 카테고리 표시
-    * 2 사용자가 입려 ㄱ받기
-    * 3 사용자가 잘못된 값 입력 처리
-     */
-
-    val categories = arrayOf("패션", "전자기기", "반려동물용품")
-
-    for(category in categories){
-        println(caterory)
-    }
-
-    println("==> 장바구니로 이동하시려면 '#'을 입력해 주세요.")
-
-    //카테고리 입력 받기
-    var selectedCategory = readLine()
-
-    //사용자가 올바른 입력을 할때가지 반복 처리
-
-    //널이 아니거나  빈공백을 입력하지 앟을 때까지지
-   while (selectedCategory.isNullOrBlank()){
-       println("값을 입력해 주세요")
-       selectedCategory = readLine()
-   }
-
-    if(selectedCategory == "#") {
-        //TODO(1)장바구니로 이동
-
-
-
-    }else {
-        //TODO(2)카테고리 상품 목록 보여주기
-        //TODO(3)카테고리 상품 목로이 없는 경우 처리
-
-
-    }
+    shoppingHome.start()
 }
