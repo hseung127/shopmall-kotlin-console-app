53105debaf7256c8263acd72ad882defe9f61895
rubat0 <rubato00@naver.com>
2022-02-03 10:53:38 +0900

    단계3-1. Home 화면과 Category 화면 분리 및 Product 클래스 추가 후 상품 목록 출력

---
+++ src/main/kotlin/data/Product.kt
@@ -0,0 +1,7 @@
+package data
+
+data class Product (
+    val categoryLable: String, //변경될 일이 없어서 val
+    val name: String, //트레일링? 콤마 뒤에 다른 항목 추가 할 때 쉽게 할 수 잇ㄱ ㅔ 하는 거 js 처럼
+
+)
--- src/main/kotlin/screen/ShoppingHome.kt
+++ src/main/kotlin/screen/ShoppingHome.kt
@@ -1,7 +1,6 @@
 package screen
 
 class ShoppingHome {
-
     /*
      * 쇼핑몰 프로그램의 시작 위치
      */
@@ -29,43 +28,9 @@ class ShoppingHome {
         )
     }
 
+    private fun showCategories() {
+        val shoppingCategory = ShoppingCategory()
 
-    fun showCategories() {
-        /*
-         * (1) 상품 카테고리 표시
-         * (2) 사용자 입력 받기
-         * (3) 사용자가 잘못된 값 입력 처리
-         */
-        val categories = arrayOf("패션", "전자기기", "반려동물용품")
-        for (category in categories) {
-            println(category)
-        }
-        println("==> 장바구니로 이동하시려면 '#'을 입력해 주세요.")
-
-        // 카테고리 입력 받기
-        var selectedCategory = readLine()
-
-        // 사용자가 옳바른 입력할때까지 반복처리
-        while (selectedCategory.isNullOrBlank()) {
-            println("값을 입력해 주세요!")
-            selectedCategory = readLine()
-        }
-
-        if (selectedCategory == "#") {
-            // TODO (1) 장바구니로 이동
-        } else {
-            if (categories.contains(selectedCategory)) {
-                // TODO (2) 카테고리 상품 목록 보여주기
-            } else {
-                showErrorMessage(selectedCategory)
-            }
-        }
+        shoppingCategory.showCategories()
     }
-
-    fun showErrorMessage(selectedCategory: String?) {
-        println("[$selectedCategory] : 존재하지 않는 카테고리입니다. 다시 입력해 주세요!")
-        showCategories()
-    }
-
-
 }
